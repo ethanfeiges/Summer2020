e2c36b2dc1ea7722d41a90467834b74ff60e1928
Ethan Feiges <ejfeiges@hotmail.com>
2021-03-21 16:21:58 -0700

    first commit

---
+++ Java3/lib/java.bat
@@ -0,0 +1,6 @@
+set CLASSPATH=.;C:\Users\ejfei\OneDrive\Documents\Java\Lib\javaintro-00-01.jar;
+set CLASSPATHDIR=.;C:\Users\ejfei\OneDrive\Documents\Java\Lib\;
+
+set Path=C:\Program Files (x86)\Common Files\Oracle\Java\javapath;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;C:\Program Files (x86)\Notepad++;c:\Program Files\Java\jdk1.8.0_251\bin;C:\Users\ejfei\OneDrive\Documents\Java\Lib;C:\Users\ejfei\AppData\Local\Microsoft\WindowsApps;
+set JAVA_HOME=c:\Program Files\Java\jdk1.8.0_251\bin
+cd %javawork%
